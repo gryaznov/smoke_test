0a36aa8622ef514d83c5b6ea336566728dffbee0
Stanislav Gryaznov <st.gryaznov@gmail.com>
2024-01-18 13:24:13 +0200

    Use Task for concurrency.

--- lib/concurrency.ex
+++ lib/concurrency.ex
@@ -4,27 +4,11 @@ defmodule Concurrency do
   """
   @spec seed((... -> any()), [any()], non_neg_integer()) :: any()
   def seed(fun, args, count) do
-    Enum.map(
-      1..count,
-      fn _ ->
-        self_pid = self()
-        spawn(fn -> send(self_pid, apply(fun, args)) end)
-      end
-    )
+    Enum.map(1..count, fn _ -> Task.async(fn -> apply(fun, args) end) end)
   end
 
   @spec harvest(any()) :: [any()]
   def harvest(list) do
-    Enum.map(
-      list,
-      fn _ ->
-        receive do
-          {status, time} -> {status, time}
-        after
-          # in case of throttling
-          1000 -> {:error, 1000}
-        end
-      end
-    )
+    Enum.map(list, &Task.await(&1))
   end
 end
